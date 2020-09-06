5fa5de00e21a811130f0b6cda335d612f74b7d75
Helka <grc.helka@gmail.com>
2020-09-06 18:03:21 +0300

    added 5 tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,21 +1,39 @@
-/*Задача 3-2
-Подсчет количества свойств в объекте
-Напиши функцию countProps(obj), которая возвращает число - количество свойств в объекте.
-Циклы не должны использоваться*/
+/*Callback функция и метод push
+Функция isUniq принимает три параметра 
+- element, index и arr. 
+Функция возвращает true или false в зависимости от того встречается ли элемент первый раз в массиве (true) 
+или этот элемент в массиве уже встречался (false) .
 
-const countProps = function (obj) {
+Функция isEven принимает один параметр 
+- element. Функция возвращает true или false 
+в зависимости от того является ли элемент четным числом или нет.
+
+Функция filterArray(array, cb), принимает 1-м параметром array 
+- массив чисел, а вторым параметром cb - функцию обратного вызова (callback). 
+Дополни тело функции так, чтобы функция filterArray заполняла новый пустой массив numbers 
+только теми элементами из массива array, для которых вызов функции cb вернет true.*/
+
+const isUniq = (element, index, arr) => arr.indexOf(element) === index;
+const isEven = (element) => element % 2 === 0;
+
+function filterArray(array, cb) {
   "use strict";
-  // Write code under this line
-  let sumKeys = 0;
-  const objectKeys = Object.keys(obj);
-  sumKeys = objectKeys.length;
-  return sumKeys;
-};
+  const numbers = [];
+  for (let i = 0; i < array.length; i += 1) {
+    const element = array[i];
+    const index = i;
+    // Write code under this line
+    if (cb(element, index, array)) {
+      numbers.push(element);
+    }
+  }
+  return numbers;
+}
 
-//return Object.keys(obj).length;
+const arr = [1, 2, 3, 4, 5, 1, 2, 5];
 
-console.log(countProps({})); // 0
+//console.log(filterArray(arr, isUniq));
+// [1, 2, 3, 4, 5]
 
-console.log(countProps({ a: 1, b: 1 })); // 2
-
-console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
+//console.log(filterArray(arr, isEven));
+// [2, 4, 2]
